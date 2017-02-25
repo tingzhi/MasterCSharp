677e19fa3f8a002d450e478e1d324d85195cc17d
Tingzhi Li <tingzhi.li6@gmail.com>
2017-02-24 17:38:35 -0800

    Halfway through grade book project and added unit tests

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// General structure: constructors, methods, and fields.
+namespace Grades
+{
+    public class GradeBook
+    {
+        /* State or Behavior*/
+        /*
+           State: The grades a user entered
+           Behavior: Add a new grade or Calculate statistics        
+        */
+
+        public GradeBook()
+        {
+            _grades = new List<float>();    // Initiate floating point list
+        }
+
+        public void AddGrade(float grade)       // This is a method
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                _grades.Add(grade);
+            }
+        }
+
+        public GradeStatistics ComputerStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0f;
+
+            foreach(float grade in _grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / _grades.Count;
+            return stats;
+        }
+
+        // This is a field
+        public string Name;
+        private List<float> _grades;    // List needs to be constructed first, see GradeBook constructor
+
+        /* A list of floating point numbers. 
+  Internal variable starts with a _ and lower-case first letter.
+*/
+    }
+}
--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -3,13 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+/*
+    Requirement: An electronic grade book to read the scores of an individual student 
+    and then compute some statistics from the scores.
+
+    The grades are entered as floating point numbers from 0 to 100, and the statistics
+    should show us the highest grade, the lowest grade, and the average grade.
+
+    Keywords: grade book, add grade, and compute statistics.
+*/
 
 namespace Grades
 {
     class Program
-    {
+    {     
         static void Main(string[] args)
         {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Hello!");
+
+            Arrays();
+
+
+            //GradeBook book = new GradeBook();   // Create a GradeBook object
+            //book.AddGrade(91f);
+            //book.AddGrade(89.5f);
+            //book.AddGrade(75f);
+
+            //GradeStatistics stats = book.ComputerStatistics();
+            //Console.WriteLine(stats.AverageGrade);
+            //Console.WriteLine(stats.HighestGrade);
+            //Console.WriteLine(stats.LowestGrade);
         }
+
+        private static void Arrays()
+        {
+            float[] grades;  // List<float> _grades
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            foreach (float grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+
+        private static void AddGrades(float[] grades)
+        {
+            grades[0] = 91f;
+            grades[1] = 98f;
+            grades[2] = 86f;
+        }
+
     }
 }
